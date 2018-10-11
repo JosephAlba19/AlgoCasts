fe17e689eededa1be0d98e28706cdc001d728917
Joseph Alba <jalba19@gmail.com>
2018-10-11 09:41:05 -0400

    completed palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// SOLUTION #1
+
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+
+//     return str === reversed;
+// }
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
+// SOLUTION #2
+
+
 
 module.exports = palindrome;
